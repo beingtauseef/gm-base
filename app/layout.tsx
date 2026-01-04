9d06104614270211326d353e3ab92e0c23e0b971
Tauseef Pathan <88334747+beingtauseef@users.noreply.github.com>
2026-01-05 02:49:27 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export const metadata: Metadata = {
     ...frameMetadata,
     "fc:frame": miniAppMetadata,
     "fc:miniapp": miniAppMetadata,
-    "base:app_id": "paste-your-app_id-here-inside-the-quotes",
+    "base:app_id": "695ad90a4d3a403912ed8d42",
   },
 };
 
